63b4459074e5c5467f79622c7652dad9027c412f
khalifmo <93163170+khalifmo@users.noreply.github.com>
2025-07-22 21:41:54 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,11 +1,10 @@
-
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from .models import Article
 
 class ArticleListView(ListView):
     model = Article
-    template_name = "core/home.html"
+    template_name = "core/home.html"  # Make sure this template exists
     context_object_name = "articles"
     paginate_by = 10
     ordering = ["-published_at"]
@@ -13,5 +12,3 @@ class ArticleListView(ListView):
 def article_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, "core/article_detail.html", {"article": article})
-
-
