0e778d8bf773b8938b71c77d4af58875b6bded1b
khalifmo <93163170+khalifmo@users.noreply.github.com>
2025-07-22 23:29:01 +0100

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,3 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -9,8 +8,11 @@ urlpatterns = [
     path("", include("core.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files during development and production
+if settings.STATIC_URL and settings.STATIC_ROOT:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+if settings.MEDIA_URL and settings.MEDIA_ROOT:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
