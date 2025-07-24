8cb519514f3150a0f85e4426e20b54ecc9996b95
khalifmo <93163170+khalifmo@users.noreply.github.com>
2025-07-25 00:47:01 +0100

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 import logging
 from django.views.generic import ListView
-from .models import Article  # Or whatever model you're using
+from django.shortcuts import render, get_object_or_404
+from .models import Article
 
 logger = logging.getLogger(__name__)
 
@@ -13,3 +14,7 @@ class ArticleListView(ListView):
         queryset = super().get_queryset()
         logger.info(f"Found {queryset.count()} articles.")
         return queryset
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, "core/article_detail.html", {"article": article})
