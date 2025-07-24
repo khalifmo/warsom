84cf06b9adbe985a22fb1d867c4b322794f80983
khalifmo <93163170+khalifmo@users.noreply.github.com>
2025-07-25 00:22:29 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ArticleListView
+from .views import ArticleListView, article_detail
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='home'),
+    path('article/<int:pk>/', article_detail, name='article_detail'),
 ]
